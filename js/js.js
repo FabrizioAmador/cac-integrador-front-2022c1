bdc6726ed0fd2e5fa3725e40d589be9fb2183c45
Fabrizio <amadorfabrizio@gmail.com>
2022-05-14 14:07:43 -0300

    Problema con .value y .onclick

--- js/js.js
+++ js/js.js
@@ -5,22 +5,22 @@ let trainee = 0.5;
 let junior = 0.85;
 let entrada = 200;
 
-if(estudiante) {
-    total = entrada * cant * estudiante;    
-} else if (trainee) {
-    total = entrada * cant * trainee;    
-} else if (junior) {
-    total = entrada * cant * estudiante;    
-} else {
-    total = entrada * cant;    
+function calcularPrecio(evt) {
+    switch (descuento) {
+        case (descuento == "estudiante"):
+            total = entrada * cant * estudiante;
+            break;
+        case (descuento == "trainee"):
+            total = entrada * cant * trainee;
+            break;
+        case (descuento == "junior"):
+            total = entrada * cant * junior;
+            break;
+        default:
+            total = entrada * cant;
+    }
+    let total
+    console.log(total)
 }
-switch (descuento){
-    case (descuento == estudiante): total = entrada * cant * estudiante;
-    break;
-    case (descuento == trainee): total = entrada * cant * trainee;
-    break;
-    case (descuento == junior): total = entrada * cant * junior;
-    break;
-    default: total = entrada * cant;
-    break;
-}
+
+document.getElementById("resumen").onclick = calcularPrecio;
